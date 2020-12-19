1b73e313da8f223fa50833efd29b0f3d4cc8dee6
febbypiter27 <febbypiter27@gmail.com>
2020-12-19 15:27:58 +0700

    first commit

---
+++ hotel_bkt/admin/inc/header.php
@@ -0,0 +1,13 @@
+
+  <header class="header black-bg">  
+	  <div class="sidebar-toggle-box">
+		  <div class="fa fa-bars tooltips" data-placement="right" data-original-title="Toggle Navigation">
+		  </div>
+		</div>         
+    <!-- <a class="logo"><p><b>W</b>EB<b style="font-size: 17px">GIS</b>&nbspADMIN</p></a> -->
+    <a class="logo" style="color:black;"><p>WebGIS Hotel (15 1811522007 Febby Putri Milenia Piter)</p></a>
+      <ul class="nav pull-right top-menu">
+        <a href="act/logout.php" class="logo" style="font-size:14px"><i class="fa fa-sign-in"></i>
+        <b>Logout</b></a>
+      </ul>
+  </header>        
